b9a2b7a98b3cbca3cfe371ec7fa8bc7458a15333
Taylor Wirtz <twirtz@thriftbooks.com>
2019-08-05 22:55:17 -0700

    player profile models

---
+++ PubLeagueTracker/Models/PlayerProfile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PubLeagueTracker.Models
+{
+    public class PlayerProfile
+    {
+        public int PlayerProfileId { get; set; }
+        public int TeamId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+    }
+}
---
+++ PubLeagueTracker/Models/ViewModels/PlayerProfiles/CreateUpdatePlayerProfileViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PubLeagueTracker.Models.ViewModels.PlayerProfiles
+{
+    public class CreateUpdatePlayerProfileViewModel
+    {
+        public IEnumerable<League> Leagues { get; set; }
+        public IEnumerable<Season> Seasons { get; set; }
+        public IEnumerable<Team> Teams { get; set; }
+        public PlayerProfile PlayerProfile { get; set; }
+    }
+}
